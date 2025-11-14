fbdd3bac78d86a82d1401997da596b425e329b6b
JVYL <jean.yoshida@outlook.com>
2025-11-14 16:28:09 -0300

    feat: implement external configuration with .env and environment variables

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 
 const connection = mysql
   .createPool({
-    host: process.env.MYSQL_HOST || "localhost",
-    port: process.env.MYSQL_PORT || 3069,
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "root",
-    database: process.env.MYSQL_DATABASE || "poke_db",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
